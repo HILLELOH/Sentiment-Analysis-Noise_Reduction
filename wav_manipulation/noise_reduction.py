6d358d5eba6a05c8ecfcf2fec9bd02e50e629360
HILLELOH <hilleloh@gmail.com>
2023-01-03 19:02:28 +0200

    evaluate class for wav manipulation

--- wav_manipulation/noise_reduction.py
+++ wav_manipulation/noise_reduction.py
@@ -1,28 +1,17 @@
 import os
+import tempfile
 import wave
-import librosa as librosa
+from random import random
 
-wav_dir = {}
-iterable_wav_dir = os.listdir(wav_dir)
+import librosa
+import soundfile
+from pydub import AudioSegment
+import numpy as np
+
+wav_dir = ""
 changes = {}  # a dict which contain {key: value} as {name_noise: list_of_files}
 count_changes = {}  # a dict which contain {key: value} as {name_noise: amount_of_files_changed}
 path_noises = ""
-
-
-def set_changes(noise_name, file_path):
-    """
-    input:
-        noise_name: path to current noise
-        file_path: path to audio file that h
-
-    """
-    if noise_name in changes.values():
-        changes[noise_name].append(file_path)  # add file of noise do changes
-        count_changes[noise_name] += 1  # add 1  to the counter
-
-    else:
-        changes[noise_name].append(file_path)  # add file of noise to list of files in current noise_name
-        count_changes[noise_name] = 1  # add 1  to the counter
 
 
 def get_duration(wav_file):
@@ -80,19 +69,132 @@ def size_seg(number):
     return segment_size, bool
 
 
+def update_noise(current_noise):
+    """
+    input:
+        current_noise: path to noise
+
+    output:
+        noise: the next noise in the directory of noises
+    """
+    flag_current = False
+    for noise in os.listdir(path_noises):
+        if flag_current:
+            return noise
+
+        if current_noise == noise:
+            flag_current = True
+
+
+def resize_noise(wav_path, duration_seconds):
+
+    audio, sr = librosa.load(wav_path, sr=44100)
+    splited_filename = audio[:int(duration_seconds*sr)]  # Truncate the audio signal to the desired duration
+
+    path_temp_noises = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\temp_noises"
+    wav_name_with_type = wav_path.split("\\")[-1]
+    wav_name = wav_name_with_type.split(".")[-1]
+    path = os.path.join(path_temp_noises, f'{wav_name}_noised.wav')
+    print(path)
+    soundfile.write(path, splited_filename, sr)  # Write the truncated audio signal to the temporary WAV file
+    return path
+
+
 def dir_noiser_holdout():
-    # create the same audio directory but with amount_noise we choose the noises that we want in list of noises
-    size_seg_ret, bool = size_seg(count_dir(path_noises))
+    """
+    the function return a directory which contain the dataset but with the noise.
+    the 'size_seg' return segment_size which represent the size of each group.
+    group is segment of the audio dir, all the group are equals.
+    each group will be colored with one of the noises that in the "noises" folder.
+    """
+
+    global new_directory_path, noise, noise_arr_new
+    if not os.path.exists(os.path.join(os.path.dirname(wav_dir), f'{os.path.basename(wav_dir)}_noised')):
+        parent_directory = os.path.dirname(wav_dir)  # Get the parent directory of the given path
+        new_directory_name = f'{os.path.basename(wav_dir)}_noised'
+
+        new_directory_path = os.path.join(parent_directory,
+                                          new_directory_name)  # Create the new directory by joining the parent directory with the new directory name
+        os.mkdir(new_directory_path)  # Create the new directory
+
+    size_seg_ret, flag_plus = size_seg(count_dir(path_noises))
 
     name_main_dir = os.path.dirname(wav_dir)
     name = f'{name_main_dir}_noised'
-    os.mkdir()
-    for w_file in iterable_wav_dir:
-        noise = ""
-        audio_arr, sr = librosa.load(w_file)  # get the array of numbers which represents the audio
-        noise_arr, sr = librosa.load(noise)
-        noise_audio = audio_arr + noise_arr
-        w_file_name_without_type = w_file.split(".")[0]
-        path_to_noise = f'{name}\\{w_file_name_without_type}_{noise}.wav'
-        librosa.output.write_wav(path_to_noise, noise_audio, sr)  # Save the mixed audio data as a new audio file
-        set_changes(noise, path_to_noise)
+
+    counter = 0
+    noise_path = os.path.join(path_noises, os.listdir(path_noises)[0])
+
+    for w_file in os.listdir(wav_dir):
+        w_file_path = os.path.join(wav_dir, w_file)
+
+        if flag_plus:
+            flag_plus = False
+            continue
+
+        if counter == size_seg_ret:
+            noise = update_noise(os.listdir(path_noises)[0])
+            noise_path = os.path.join(os.path.dirname(path_noises), noise)
+            counter = 0
+
+        if get_duration(w_file_path) < get_duration(noise_path):
+            new_noise_path = resize_noise(noise_path, int(get_duration(w_file_path)))
+            audio_arr, sr1 = librosa.load(w_file_path)  # get the array of numbers which represents the audio
+            noise_arr, sr2 = librosa.load(new_noise_path)
+
+            if sr1 != sr2:
+                noise_arr_new = librosa.resample(noise_arr, sr2, sr1)  # Resample y2 to the same sample rate as y1
+
+            # Concatenate the audio data arrays
+            new_noised = np.concatenate((audio_arr, noise_arr_new))
+
+            path_to_noise = f'{new_directory_path}\\{w_file}'
+            # Write the concatenated audio data to a new WAV file
+            soundfile.write(path_to_noise, new_noised, sr1)
+            noise_audio = [sum(x) for x in zip(audio_arr, noise_arr_new)]
+            soundfile.write(path_to_noise, noise_audio, sr1)  # Save the mixed audio data as a new audio file
+            counter += 1
+
+        elif get_duration(w_file) >= get_duration(noise_path):
+            audio_arr, sr1 = librosa.load(w_file, sr=44100)  # get the array of numbers which represents the audio
+            noise_arr, sr2 = librosa.load(noise, sr=44100)
+            if sr1 != sr2:
+                noise_arr = librosa.resample(noise_arr, sr2, sr1)  # Resample y2 to the same sample rate as y1
+            noise_audio = [sum(x) for x in zip(audio_arr, noise_arr)]
+            path_to_noise = f'{new_directory_path}\\{w_file_path}'
+
+            soundfile.write(path_to_noise, noise_audio, sr1)  # Save the mixed audio data as a new audio file
+            counter += 1
+
+
+if __name__ == '__main__':
+    # noise_path = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\wav_manipulation\\noises"
+    # dataset_clean = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_02"
+    #
+    # wav_dir = dataset_clean
+    # path_noises = noise_path
+    # dir_noiser_holdout()
+################################
+    w_file = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_02\\02_01_01_01_dogs-sitting_disgust.wav"
+    noise = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\wav_manipulation\\noises\\baby_crying.wav"
+    try_path = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_02"
+    path_to_noise = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_02_noised"
+
+    if get_duration(noise) > get_duration(w_file):
+        resize_noise(noise, get_duration(w_file))
+        print("ok")
+
+    audio_arr, sr1 = librosa.load(w_file, sr=44100)  # get the array of numbers which represents the audio
+    noise_arr, sr2 = librosa.load(noise, sr=44100)
+    print(audio_arr)
+    if sr1 != sr2:
+        noise_arr = librosa.resample(noise_arr, sr2, sr1)  # Resample y2 to the same sample rate as y1
+
+    noise_audio = [sum(x) for x in zip(audio_arr, noise_arr)]
+
+    path_noise = f'{path_to_noise}\\new_noise.wav'
+    soundfile.write(path_noise, noise_audio, sr1)
+################################
+    # noise = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\wav_manipulation\\noises\\baby_crying.wav"
+    # str = resize_noise(noise, 16)
+    # print(str)
--- wav_manipulation/wav_cut.py
+++ wav_manipulation/wav_cut.py
@@ -8,61 +8,24 @@ def single_split_by_ms(from_ms, to_ms, split_filename, file_path):
     audio = AudioSegment.from_wav(filepath)
     new_split_filename = audio[int(from_ms):int(to_ms)]
     interval = f'_{from_ms}_{to_ms}'  # mcreate for the path of the new file
-    my_path = "C:\\Users\\hille\\Documents\\University\\third year\\first semster\\language procces and sound blocks\\wav_file\\" + split_filename.split('.')[0] + interval + ".wav"
+    my_path = "C:\\Users\\hille\\Documents\\University\\third year\\first semster\\language procces and sound blocks\\wav_file\\" + \
+              split_filename.split('.')[0] + interval + ".wav"
     new_split_filename.export(my_path, format="wav")
-#'21:34:14.245'
+
+
+# '21:34:14.245'
 
 def h_to_ms(str_hour):
     save_string_hour = str_hour
-    str_hour = str_hour.split(':')  # get the three parts: hour, minute, second+ms **(the seconds and ms will be split by '.')
+    str_hour = str_hour.split(
+        ':')  # get the three parts: hour, minute, second+ms **(the seconds and ms will be split by '.')
     hours = int(str_hour[0]) * 1000 * 60 * 60
     minutes = int(str_hour[1]) * 1000 * 60
-    seconds = int((str_hour[2].split('.'))[0])* 1000 * 1
+    seconds = int((str_hour[2].split('.'))[0]) * 1000 * 1
     ms = int((str_hour[2].split('.'))[1]) * 1
     sum = hours + minutes + seconds + ms
     # the sum of the ms we convert hour to ms + min to ms + second to ms + ms to ms (which is the specific amount of ms)
     return (sum, save_string_hour)
-
-#there are 60*60*1000 ms in one hour
-#there are 60*1000 ms in one minutes
-#there are 1000 ms in one second
-'''
-def ms_to_string(ms):
-    #calculate hour
-    hour = 0
-    if ms >= 60*60*1000: #if the ms can create an hour and if not : hour = 0
-        while ms - 3600 * 1000 * hour > 0: #if the ms can create more that one hour and return the total hours
-            hour += 1
-    tot_h = hour
-    ms = ms - (60 * 60 * 1000 * tot_h)
-    print(f'ms - (60 * 60 * 1000 * tot_h) = {ms}' )
-
-    # calculate minute
-    minute = 0
-    if ms >= 60*1000:
-        while ms - 60 * 1000 * minute > 0:
-            minute += 1
-    tot_m = minute
-    ms = ms - (60 * 1000 * tot_m)
-    print(f'ms - (60 * 1000 * tot_m) = {ms}')
-
-    # calculate second
-    second = 0
-    if ms >= 1000:
-        while ms - 1000 * second > 0:
-            second += 1
-    tot_s = second
-    ms = ms - (1000 * tot_s)
-    print(f'ms - (1000 * tot_s) = {ms}')
-
-    # calculate ms
-    tot_ms = ms
-    print(f'ms = {ms}')
-
-    print(f'{tot_h}:{tot_m}:{tot_s}:{tot_ms}')
-    return f'{tot_h}:{tot_m}:{tot_s}:{tot_ms}'
-'''
-
 
 
 def multi_split(list_of_times, audio_file, audio_path):  # give a list of time's interval which convert to ms and split.
@@ -72,7 +35,9 @@ def multi_split(list_of_times, audio_file, audio_path):  # give a list of time's
 
 
 def convert_csv(path_csv_file):
-    csv_df = pd.read_csv(path_csv_file, names= ['Utterance', 'Speaker', 'Emotion', 'Scene_ID', 'Utterance_ID', 'Season', 'Episode', 'Start_Time', 'End_Time'], skiprows=[0])
+    csv_df = pd.read_csv(path_csv_file,
+                         names=['Utterance', 'Speaker', 'Emotion', 'Scene_ID', 'Utterance_ID', 'Season', 'Episode',
+                                'Start_Time', 'End_Time'], skiprows=[0])
     utterance = csv_df['Utterance'].tolist()
     start_Time = csv_df['Start_Time'].tolist()
     end_Time = csv_df['End_Time'].tolist()
@@ -81,13 +46,14 @@ def convert_csv(path_csv_file):
     print(end_Time)
     print(h_to_ms(start_Time[2]))
 
+
 if __name__ == '__main__':
     folder = "C:\\Users\\hille\\Documents\\University\\third year\\first semster\\language procces and sound blocks\\wav_file"
     file = "megadeth.wav"
 
     # spliter.single_split_by_ms(0, (spliter.get_duration()/2)*1000, file, folder)
-    #multi_split([4, 800, 3, 323, 2345, 3423], file, folder)
+    # multi_split([4, 800, 3, 323, 2345, 3423], file, folder)
     h_to_ms('4:50:32.67')
     print(h_to_ms('4:50:32.67')[1])
-    #folder1 = "C:\\Users\\hille\\Documents\\University\\third year\\first semster\\language procces and sound blocks\\dataset-friends.csv"
-    #convert_csv(folder1)
+    # folder1 = "C:\\Users\\hille\\Documents\\University\\third year\\first semster\\language procces and sound blocks\\dataset-friends.csv"
+    # convert_csv(folder1)
