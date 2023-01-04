711f534e1234159da105e66ec420824f25483aaa
HILLELOH <hilleloh@gmail.com>
2023-01-04 16:24:41 +0200

    noiser working but on directories little error

--- wav_manipulation/noise_reduction.py
+++ wav_manipulation/noise_reduction.py
@@ -1,26 +1,20 @@
 import os
-import tempfile
 import wave
-from random import random
 
 import librosa
 import soundfile
-from pydub import AudioSegment
-import numpy as np
 
 wav_dir = ""
-changes = {}  # a dict which contain {key: value} as {name_noise: list_of_files}
-count_changes = {}  # a dict which contain {key: value} as {name_noise: amount_of_files_changed}
 path_noises = ""
 
 
 def get_duration(wav_file):
     """
     input:
-        wav_file: path to file
+        wav_file: (string) path to file
 
     output:
-        duration: duration of the wav file
+        duration: (int) duration of the wav file
     """
     with wave.open(wav_file, 'rb') as wf:
         frame_rate = wf.getframerate()
@@ -33,10 +27,10 @@ def get_duration(wav_file):
 def count_dir(path_dir):
     """
     input:
-        dir: path to directory
+        path_dir: (string) path to directory
 
     output:
-        count: how many files are in this directory
+        count: (int) how many files are in this directory
     """
     count = 0
     # Iterate directory
@@ -47,40 +41,46 @@ def count_dir(path_dir):
     return count
 
 
-def size_seg(number):
+def size_seg(wav_file_dir, number):
     """
     input:
-        number: int
+        wav_file_dir: (string) represent the audio directory
+        number: (int) the number of the noises
 
     output:
-        segment_size: how many groups will be if we divide the number of files by the number of exist noises
-        bool: represent if there will be a rest or not
+        segment_size: (int) how many groups will be if we divide the number of files by the number of exist noises
+        flag: (boolean) represent if there will be a rest or not
     """
-    global wav_dir
-    print(count_dir(wav_dir))
-    bool = False
-    if count_dir(wav_dir) % number == 0:
-        segment_size = count_dir(wav_dir) / number
+    global mod, side_ret
+    flag = False
+    if count_dir(wav_file_dir) % number == 0:
+        segment_size = int(count_dir(wav_file_dir) / number)
     else:
-        segment_size = count_dir(wav_dir) / number
-        bool = True
-    return segment_size, bool
+        segment_size = count_dir(wav_file_dir) / number
+        plus = segment_size - int(segment_size)  # add the modulo thar segment_size have
+        mod = int(count_dir(path_noises)*plus)
+        side_ret = int(mod/count_dir(path_noises))
+        segment_size = int(segment_size)
+        segment_size = segment_size + side_ret
+        flag = True
+    return segment_size, flag
 
 
 def update_noise(current_noise_path):
     """
     input:
-        current_noise: path to noise
+        current_noise: (string) path to noise
 
     output:
-        noise: the next noise in the directory of noises
+        noise: (string) the next noise in the directory of noises
     """
     current_noise_file = current_noise_path.split("\\")[-1]
     flag_current = False
     list_noises = os.listdir(path_noises)
 
     if current_noise_file == list_noises[-1]:
-        return 1
+        print(f'End of process: no more files!')
+        return
 
     else:
         for noise_file in list_noises:
@@ -92,18 +92,30 @@ def update_noise(current_noise_path):
 
 
 def resize_noise(wav_path, duration_seconds):
+
+    """
+    input:
+        wav_path: (string) to be cut
+        duration_seconds: (seconds) what length will be the temporary noise
+
+    return:
+          path: (string) path of the temp noise audio after adjust the time
+    """
+
     audio, sr = librosa.load(wav_path, sr=44100)
     splited_filename = audio[:int(duration_seconds * sr)]  # Truncate the audio signal to the desired duration
 
-    path_temp_noises = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\temp_noises"
     wav_name_with_type = wav_path.split("\\")[-1]
-    wav_name = wav_name_with_type.split(".")[-1]
-    path = os.path.join(path_temp_noises, f'{wav_name}_noised_{duration_seconds}.wav')
+    wav_name_without_type = wav_name_with_type.split(".")[0]
+
+    path_temp_noises = f'C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training' \
+                       f'\\temp_noises'
+    path = os.path.join(path_temp_noises, f'{wav_name_without_type}_noised_{duration_seconds}.wav')
     soundfile.write(path, splited_filename, sr)  # Write the truncated audio signal to the temporary WAV file
     return path
 
 
-def balance_sounds(wav_audio_arr, wav_noise_arr):
+def balance_sounds(wav_noise_arr):
     return wav_noise_arr / 4
 
 
@@ -122,18 +134,15 @@ def dir_noiser_holdout():
     directory_path = os.path.join(parent_directory,
                                   parent_directory_name)  # Create the new directory by joining the parent directory with the new directory name
 
-    if not os.path.exists(os.path.join(os.path.dirname(wav_dir), f'{os.path.basename(wav_dir)}_noised')):
+    if not os.path.exists(os.path.join(directory_path)):
         # create the folder which will contain the noisy data
         os.mkdir(directory_path)
 
-
-    size_seg_ret, flag_plus = size_seg(
-        count_dir(path_noises))  # find how many wav files will be selected fot each noise
-    name_main_dir = os.path.dirname(wav_dir)
-    path_to_noisy_audio = f'{name_main_dir}_noised'
+    size_seg_ret, flag_plus = size_seg(wav_dir, count_dir(path_noises))  # find how many wav files will be selected fot each noise
     print(f'{size_seg_ret}%%{flag_plus}')
 
-    counter = 0  # until the size of as segment
+    counter_segment = 0  # until the size of as segment
+    counter_all = 0
 
     noise_path = os.path.join(path_noises,
                               os.listdir(path_noises)[0])  # create the init path to noise(the first in the list)
@@ -146,10 +155,15 @@ def dir_noiser_holdout():
             flag_plus = False
             continue
 
-        if counter == int(size_seg_ret):  # get path for a new noise and init the counter
-            noise = update_noise(noise_path)
+        if counter_segment == int(size_seg_ret):  # get path for a new noise and init the counter
+            if counter_all == count_dir(wav_dir):
+                print(f'End of process: {count_dir(wav_dir)} audio files passed with noise!')
+            noise = update_noise(os.path.join(noise_path, noise))
+            if noise is None:
+                print("No more noises!")
+                return
             noise_path = os.path.join(path_noises, noise)
-            counter = 0
+            counter_segment = 0
 
         if get_duration(w_file_path) < get_duration(noise_path):
             new_noise_path = resize_noise(noise_path, int(get_duration(w_file_path)))
@@ -161,19 +175,19 @@ def dir_noiser_holdout():
             noise_arr_new = librosa.resample(noise_arr, sr2,
                                              sr1)  # Resample audio_arr to the same sample rate as noise_arr
 
-        noise_arr_new = balance_sounds(audio_arr, noise_arr)
+        noise_arr_new = balance_sounds(noise_arr)
 
         noise_audio = [sum(x) for x in zip(audio_arr, noise_arr_new)]
         path_to_noisy_audio = f'{directory_path}\\noised_{w_file}'
         soundfile.write(path_to_noisy_audio, noise_audio, sr1)  # Save the mixed audio data as a new audio file
-        print(f'{counter} new file {path_to_noisy_audio} was added with {noise} noise')
-        counter += 1
-
+        print(f'{counter_all}___{counter_segment} new file {path_to_noisy_audio} was added with {noise} noise')
+        counter_segment += 1
+        counter_all += 1
 
 if __name__ == '__main__':
     ################################
     w_file = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_02\\02_01_01_01_dogs-sitting_disgust.wav"
-    wav_dir = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_01"
+    wav_dir = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\try"
     noise = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\wav_manipulation\\noises\\heavy_rain_and_thunder.wav"
     try_path = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_02"
     path_to_noise = "C:\\Users\\hille\\PycharmProjects\\Sentiment-Analysis-Noise_Reduction\\data\\training\\Actor_02_noised"
